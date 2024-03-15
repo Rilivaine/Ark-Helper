5fb3690198ae1f3d5263bfb7c9eeb53976535ebb
Rilivaine <rilivaine@proton.me>
2024-03-15 22:32:59 +0200

    Fix empty copied message

--- scripts/utils/message.js
+++ scripts/utils/message.js
@@ -1,9 +1,12 @@
 import { callbackForRampRanges } from '../data/rampRange.js';
+import { loadDataFromLocalStorage } from '../data/dataOperations.js';
+import { parseRawRampRange } from '../data/rampRange.js';
 
 export function composeMessage(data) {
+  const rampsRanges = parseRawRampRange(loadDataFromLocalStorage('rampsRanges'));
   let message = '';
 
-  callbackForRampRanges(data, (i) => {
+  callbackForRampRanges(rampsRanges, (i) => {
     message += `${i} -> ${data?.[i].containerNumber ?? data?.[i].truckNumber ?? "-"}${data?.[i].containerNumber ? data?.[i].truckNumber ? ' | ' + data?.[i].truckNumber : "" : ""}\n`;
   });
 
